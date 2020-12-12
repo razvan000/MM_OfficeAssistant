cca0d0829da800209b5aa33594406d9208ad908e
LaurentiuMihai99 <naum_mihai2008@yahoo.com>
2020-12-12 17:18:38 +0200

    Main Page front end - No animations

---
+++ Popup.js
@@ -0,0 +1,21 @@
+var scheduleModal = document.getElementById("scheduleModal");
+var reserveModal = document.getElementById("reserveModal");
+var infoModal = document.getElementById("infoModal");
+var btnSchedule = document.getElementById("btn1");
+var btnReserve = document.getElementById("btn2");
+var btnInfo = document.getElementById("btn3");
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+btnSchedule.addEventListener("mouseover", function(){ scheduleModal.style.display = "block"; })
+btnSchedule.addEventListener("mouseout", function(){ scheduleModal.style.display = "none"; })
+
+btnReserve.addEventListener("mouseover", function(){ reserveModal.style.display = "block"; })
+btnReserve.addEventListener("mouseout", function(){ reserveModal.style.display = "none"; })
+
+btnInfo.addEventListener("mouseover", function(){ infoModal.style.display = "block"; })
+btnInfo.addEventListener("mouseout", function(){ infoModal.style.display = "none"; })
