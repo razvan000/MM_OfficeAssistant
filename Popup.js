38fc54dadd445ce800101c8323de2ebe1393a3f9
unknown <mrazvan99@yahoo.com>
2021-01-12 15:56:57 +0200

    text animation | video with controls

--- Popup.js
+++ Popup.js
@@ -4,6 +4,30 @@ var infoModal = document.getElementById("infoModal");
 var btnSchedule = document.getElementById("btn1");
 var btnChatBot = document.getElementById("btn2");
 var btnInfo = document.getElementById("btn3");
+const welcome=document.querySelector(".welcome")
+const text=welcome.textContent
+const splitText= text.split("")
+welcome.textContent=""
+for(let i=0; i<splitText.length; i++){
+  welcome.innerHTML+="<span><pre>"+splitText[i]+"</pre></span>"
+}
+
+let char=0
+let timer=setInterval(onTick, 50)
+function onTick(){
+  const span = welcome.querySelectorAll('span')[char]
+  span.classList.add('fade')
+  char++
+  if(char===splitText.length){
+    finish()
+    return
+  }
+}
+
+function finish(){
+  clearInterval(timer)
+  timer=null
+}
 
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -25,3 +49,6 @@ btnChatBot.onclick = function () {
 
 btnInfo.addEventListener("mouseover", function(){ infoModal.style.display = "block"; })
 btnInfo.addEventListener("mouseout", function(){ infoModal.style.display = "none"; })
+btnInfo.onclick = function () {
+  location.href = "Info.html";
+};
